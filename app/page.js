53c14cda67247645af1394f0ae3bf5ce04319439
Yagnik Akbari <yagnik58ppsv@gmail.com>
2024-10-21 08:39:00 +0530

    start learning the react + rest api integration of paypal

--- app/page.js
+++ app/page.js
@@ -1,25 +1,63 @@
-import { loadScript } from "@paypal/paypal-js";
-loadScript({
-  "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
-  currency: "EUR",
-})
-  .then((paypal) => {
-    console.info("paypal", paypal);
-    paypal
-      .Buttons()
-      .render("#your-container-element")
-      .catch((error) => {
-        console.error("failed to render the PayPal Buttons", error);
+"use client";
+
+import {
+  PayPalButtons,
+  PayPalCardFieldsForm,
+  PayPalCardFieldsProvider,
+  PayPalScriptProvider,
+  usePayPalCardFields,
+} from "@paypal/react-paypal-js";
+
+const initialOptions = {
+  clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+  currency: "USD",
+  intent: "capture",
+  components: "card-fields,buttons",
+};
+const SubmitPayment = () => {
+  const { cardFields, fields } = usePayPalCardFields();
+
+  function submitHandler() {
+    if (typeof cardFields.submit !== "function") return; // validate that `submit()` exists before using it
+
+    cardFields
+      .submit()
+      .then(() => {
+        // submit successful
+      })
+      .catch(() => {
+        // submission error
       });
-  })
-  .catch((err) => {
-    console.error("failed to load the PayPal JS SDK script", err);
-  });
+  }
+  return <button onClick={submitHandler}>Pay</button>;
+};
 
 export default function Home() {
+  function createOrder() {
+    // merchant code
+  }
+  function onApprove() {
+    // merchant code
+  }
+  function onError() {
+    // merchant code
+  }
   return (
-    <div className="container-paypal">
-      <button>Pay Via Paypal</button>
-    </div>
+    <>
+      <div className="container-paypal">
+        {/* <button>Pay Via Paypal</button> */}
+        <PayPalScriptProvider options={initialOptions}>
+          <PayPalButtons style={{ layout: "horizontal" }} />
+          <PayPalCardFieldsProvider
+            createOrder={createOrder}
+            onApprove={onApprove}
+            onError={onError}
+          >
+            <PayPalCardFieldsForm />
+            <SubmitPayment />
+          </PayPalCardFieldsProvider>
+        </PayPalScriptProvider>
+      </div>
+    </>
   );
 }
